5c4b6cf8536b8687542e5b136612c0ae0792c2c1
Suzanne Richman <srichm@ext.uber.com>
2019-02-22 09:52:52 -0800

    FINALLY figured out two ways to reverse an int even when negative. negative was tripping me up because of the string/byte conversion I was originally taking.Research showed no need for byte array. test showed what was happenging to negative. Found second solution too.

---
+++ src/awesomeProject/reverse_int.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	testIntP := 4345
+	testIntN := -123
+
+	fmt.Println("and the answer is: ", reverse(testIntP))
+	fmt.Println("and the answer is: ", reverse(testIntN))
+
+	fmt.Println("try reverseTwo method")
+	fmt.Println( reverseTwo(testIntP))
+	fmt.Println( reverseTwo(testIntN))
+
+}
+
+//original solution couldn't handle negatives
+//referenced this for help: https://leetcode.com/problems/reverse-integer/discuss/184798/golang
+func reverse(x int) int {
+
+	orgS := ""
+	newS := ""
+
+	//convert int to string using "i to a" method built into Go
+	if x > 0{
+		orgS = strconv.Itoa(x)
+	} else {
+		//in case X is negative
+		orgS = strconv.Itoa(-x)
+	}
+
+	fmt.Println(orgS)
+
+	for i := len(orgS)-1; i> -1; i--{
+		newS += string(orgS[i])
+	}
+
+	fmt.Println("new string", newS)
+
+	//convert string into a 32 bit int using "a to i" method built into Go which requires both an int and err as output
+	//err can be ignored by using "_" or you can use if statement to include check for err
+	newInt, err := strconv.Atoi(newS)
+	if err == nil {
+		fmt.Println(newInt)
+	}
+
+	//add negative back in at the end if part of original value
+	if x < 0{
+		newInt = -newInt
+
+		fmt.Println(newInt)
+	}
+
+	return newInt
+}
+
+
+//alt solution: USE MODULUS! (after reading discussion on leetcode)
+
+func reverseTwo (x int) int {
+
+	sum :=0
+
+	for x !=0 {
+		sum = sum*10 +x%10
+		x = x/10
+	}
+	return sum
+}
