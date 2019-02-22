cbf4c82e4ef03d036ec1c0aa889bd78745114960
Suzanne Richman <srichm@ext.uber.com>
2019-02-22 09:13:17 -0800

    adding reverse strings challenge with a couple tests added in main as test harness - still investingating how to converty returned byte arrays into char arrays in go

---
+++ src/awesomeProject/reverse_strings.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	testByteA := []byte{'h','e','l','l','o'}
+
+	fmt.Println(testByteA)
+	fmt.Println(reverseString(testByteA))
+
+	testByteB := []byte{'s','o','o','z'}
+
+	fmt.Println(testByteB)
+	fmt.Println(reverseString(testByteB))
+	
+}
+
+func reverseString(s []byte) []byte {
+
+	//declare and assign the variables for going through the array from both ends
+	i :=0
+	j:= len(s)-1
+
+	//use the go for loop like a while loop to iterate through the array
+	for i < j {
+
+		//swap values from either end of the array
+		s[i],s[j] = s[j], s[i]
+
+		//increment and decrement from both sides of the array and you'll meet in the middle
+		i++
+		j--
+	}
+
+	//return the array
+	return s
+}
