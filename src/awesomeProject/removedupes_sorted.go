0ac205893c36b2fc28b1840f60ee544d12b011fc
Suzanne Richman <srichm@ext.uber.com>
2019-04-18 16:34:31 -0700

    did removedupes

---
+++ src/awesomeProject/removedupes_sorted.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	testArr := []int{1, 1, 2}
+	fmt.Println(removeDuplicates(testArr))
+
+	test2 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates(test2))
+}
+
+// solution help from https://leetcode.com/problems/remove-duplicates-from-sorted-array/discuss/247190/Go-100-56ms
+func removeDuplicates(nums []int) int {
+
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
+	for i, j := 0, 1; i < j && j < len(nums); {
+		if nums[j] == nums[i] {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
+			j--
+		}
+		i++
+		j++
+	}
+	return len(nums)
+}
